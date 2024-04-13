c14081bdc60980e632c57110a87ea509d3de930a
Souvik Mukhopadhyay <souvik9205@gmail.com>
2024-04-13 16:18:36 +0530

    latest

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    jsxInject: `import React from 'react'`, // Add this line to inject React into JSX
-  },
-  optimizeDeps: {
-    include: ["@babel/plugin-syntax-import-attributes"], // Add this line to include the required plugin
-  },
 });
